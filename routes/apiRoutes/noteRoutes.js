d5971670e14b6cf6a3e71a876f6bf310f305d943
Cristina Dege <cris.dege0813@gmail.com>
2022-08-18 21:21:30 -0400

    notes saving dinamically

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,21 +1,28 @@
 const router = require("express").Router();
 const fs = require("fs");
+const path = require("path");
 // const { notes } = require("../../data/notes.json");
 
 fs.readFile("./data/notes.json", "utf8", (err, data) => {
   if (err) throw err;
 
-  var notes = JSON.parse(data);
+  // var notes = JSON.parse(data);
+
+  const { notes } = require("../../data/notes.json");
 
   router.get("/notes", (req, res) => {
     res.json(notes);
   });
 
   router.post("/notes", (req, res) => {
-    let newNote = req.body;
-    notes.push(newNote);
-    updateData();
-    return console.log(newNote.title + "was added!");
+    // let newNote = req.body;
+    // notes.push(newNote);
+    req.body.id = notes.length.toString();
+
+    const note = updateData(req.body, notes);
+    // updateData();
+    res.json(note);
+    // return console.log(note.title + "was added!");
   });
 
   router.get("/notes/:id", (req, res) => {
@@ -28,11 +35,20 @@ fs.readFile("./data/notes.json", "utf8", (err, data) => {
     console.log("Deleted note with id " + req.params.id);
   });
 
-  function updateData() {
-    fs.writeFile("data/notes.json", JSON.stringify(notes, "\t"), (err) => {
-      if (err) throw err;
-      return true;
-    });
+  function updateData(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+
+    // fs.writeFile("data/notes.json", JSON.stringify(notes, "\t"), (err) => {
+    //   if (err) throw err;
+    //   return newNote;
+    // });
+
+    fs.writeFileSync(
+      path.join(__dirname, "../../data/notes.json"),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return newNote;
   }
 });
 
